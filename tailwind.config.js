afb52a9b277cce381a0cd3fe87ff4e526b6270e9
justinbornais <justin.bornais@gmail.com>
2024-09-25 16:06:09 -0400

    Updated navbar and tailwind dark theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    content: ['./src/**/*.astro'],
+    content: [
+      './src/**/*.astro',
+      // 'node_modules/flowbite/**/*.js'
+    ],
     theme: {
       extend: {},
     },
-    plugins: [],
+    darkMode: 'media',
+    plugins: [require('flowbite/plugin')],
   };
